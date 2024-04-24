899fc9be8c53c0bf1275b4a5241102a353b4c758
LeandreLeNulzec <leandre.le.nulzec@gmail.com>
2024-04-24 22:38:16 +0200

    première version du PianoRoll

--- app/src/main/java/poolongprojectn7/App.java
+++ app/src/main/java/poolongprojectn7/App.java
@@ -1,10 +1,16 @@
 package poolongprojectn7;
 
 import javafx.application.Application;
+import javafx.stage.*;
 
-public class App {
+public class App extends Application{
     
     public static void main(String[] args) {
-        
+        launch(args);
+    }
+
+    public void start(Stage stage) {
+        PianoRoll p = new PianoRoll();
+        p.start(stage);
     }
 }
