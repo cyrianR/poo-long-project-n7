96a28a10b913fc2de342c53510450175831d8f71
mar1karaoui <marwanekaraoui20@gmail.com>
2024-04-26 19:23:29 +0200

    interface graphique ToolBar

--- app/src/main/java/poolongprojectn7/App.java
+++ app/src/main/java/poolongprojectn7/App.java
@@ -1,10 +1,25 @@
 package poolongprojectn7;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-public class App {
-    
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Tool_Bar toolBar = new Tool_Bar();
+        BorderPane root = new BorderPane();
+        root.setTop(toolBar.getToolbar());
+
+        Scene scene = new Scene(root, 800, 600);
+        primaryStage.setTitle("N7 Music Studio");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        
+        launch(args);
     }
 }
