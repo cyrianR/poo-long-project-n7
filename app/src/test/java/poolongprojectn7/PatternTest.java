39c5846f467745a08bd8c6222274aeefc81b61e8
cyrianR <ragotcyrian@protonmail.com>
2024-04-28 12:29:44 +0200

    fix: remove instrument from test

--- app/src/test/java/poolongprojectn7/PatternTest.java
+++ app/src/test/java/poolongprojectn7/PatternTest.java
@@ -12,17 +12,14 @@ class PatternTest {
 
     @Test
     public void testConstructorAndGetters() {
-        Instrument instrument = null;
-        Pattern pattern = new Pattern(instrument);
-        assertEquals(instrument, pattern.getInstrument());
+        Pattern pattern = new Pattern();
         assertNotNull(pattern.getSequence());
         assertEquals(0, pattern.getPatternLength());
     }
 
     @Test
     public void testAddNote() throws InvalidMidiDataException {
-        Instrument instrument = null;
-        Pattern pattern = new Pattern(instrument);
+        Pattern pattern = new Pattern();
         Note note = new Note(3, 7, 100, 500);
         pattern.addNote(note, 20);
         pattern.addNote(note, 120);
