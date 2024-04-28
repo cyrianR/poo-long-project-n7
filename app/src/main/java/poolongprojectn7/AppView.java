48d8a5b81fd7f7f53951622d1154556901038553
Nell15 <nelltruong67@gmail.com>
2024-04-28 17:25:59 +0200

    updates toolbar for marwane's

--- app/src/main/java/poolongprojectn7/AppView.java
+++ app/src/main/java/poolongprojectn7/AppView.java
@@ -2,7 +2,7 @@ package poolongprojectn7;
 
 import javafx.scene.layout.VBox;
 import poolongprojectn7.pianoroll.PianoRoll;
-import javafx.scene.control.Button;
+import poolongprojectn7.toolbarcomponent.ToolBarComponent;
 import javafx.scene.control.ToolBar;
 
 public class AppView extends VBox {
@@ -10,14 +10,10 @@ public class AppView extends VBox {
     public AppView(AppModel model, Runnable handler) {
 
         // Creating toolbar
-        ToolBar ToolBar = new ToolBar();
-        Button Button = new Button("Test");
-        ToolBar.getItems().add(Button);
-        Button Button2 = new Button("Button 2");
-        ToolBar.getItems().add(Button2);
-        PianoRoll Piano = new PianoRoll();
+        ToolBar toolBar = new ToolBarComponent().getToolbar();
+        PianoRoll piano = new PianoRoll();
 
-        VBox vBox = new VBox(ToolBar, Piano);
+        VBox vBox = new VBox(toolBar, piano);
 
         getChildren().addAll(vBox);
     }
