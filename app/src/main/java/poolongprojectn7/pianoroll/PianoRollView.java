f1be92fb7d28d0ce7f2cb6f806c723efdb7927fb
cyrianR <149821230+cyrianR@users.noreply.github.com>
2024-05-13 23:57:11 +0200

    refactor(pianoroll): start refactor (#35)

---
+++ app/src/main/java/poolongprojectn7/pianoroll/PianoRollView.java
@@ -0,0 +1,16 @@
+package poolongprojectn7.pianoroll;
+
+import javafx.scene.layout.HBox;
+
+public class PianoRollView extends HBox{
+    
+    private PianoRollModel model;
+
+    public PianoRollView(PianoRollModel model) {
+        this.model = model;
+
+        
+    }
+
+
+}
