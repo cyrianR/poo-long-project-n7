00ccf4e03887d48ff6f3a4d3e5e9925cac6c5c7a
cyrianR <ragotcyrian@protonmail.com>
2024-05-13 23:51:39 +0200

    refactor(pianoroll): start refactor

---
+++ app/src/main/java/poolongprojectn7/pianoroll/PianoRollModel.java
@@ -0,0 +1,15 @@
+package poolongprojectn7.pianoroll;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
+public class PianoRollModel {
+
+    private final String[] NOTE_LETTERS = {"C","C#","D","D#","E","F","F#","G","G#","A","A#","B"};
+    private final IntegerProperty currentOctave = new SimpleIntegerProperty(4);
+
+    
+}
