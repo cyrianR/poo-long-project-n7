263baae49bafbdc03cdfb8fd63e55ca1aaf9f538
vic_pabo <viclouisnoe@gmail.com>
2024-05-26 20:12:01 +0200

    Multiple sounds in same time fix

--- app/src/main/java/poolongprojectn7/Playlist.java
+++ app/src/main/java/poolongprojectn7/Playlist.java
@@ -1,7 +1,6 @@
 package poolongprojectn7;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -13,8 +12,11 @@ import java.util.concurrent.TimeUnit;
  */
 public class Playlist {
 
+    /* Number of threads available */
+    private final int NB_THREADS = 10;
+
     /* The sounds of the playlist. */
-    private Map<SoundEntry, Integer> soundMap;
+    private Map<SoundEntry, List<Integer>> soundMap;
     /* The scheduler of the playlist. */
     private ScheduledExecutorService scheduler;
 
@@ -23,26 +25,30 @@ public class Playlist {
      */
     public Playlist() {
         this.soundMap = new HashMap<>();
-        this.scheduler = Executors.newScheduledThreadPool(1);
+        this.scheduler = Executors.newScheduledThreadPool(NB_THREADS);
     }
 
     /**
      * Add a wav sound to the playlist.
      * @param sound the sound to add
-     * @param delay the time the sound begins to be played in the playlist
+     * @param delay the time in milliseconds the sound begins to be played in the playlist
      */
     public void addSound(SoundEntry sound, int delay) {
-        this.soundMap.put(sound, delay);
+        soundMap.computeIfAbsent(sound, k -> new ArrayList<>()).add(delay);
     }
 
     /**
      * Play all the sounds of the playlist.
      */
     public void playAll() {
-        for (Map.Entry<SoundEntry, Integer> entry : soundMap.entrySet()) {
-            scheduler.schedule(entry.getKey()::play, entry.getValue(), TimeUnit.MILLISECONDS);
+        for (Map.Entry<SoundEntry, List<Integer>> entry : soundMap.entrySet()) {
+            SoundEntry sound = entry.getKey();
+            for (int delay : entry.getValue()) {
+                this.scheduler.schedule(() -> sound.play(), delay, TimeUnit.SECONDS);
+            }
         }
-        scheduler.schedule(() -> scheduler.shutdown(), getTotalDuration(), TimeUnit.MILLISECONDS);
+        /* Schedule the shutdown of the scheduler with the adjusted total duration according to the BPM */
+        this.scheduler.schedule(() -> scheduler.shutdown(), getTotalDuration(), TimeUnit.SECONDS);
     }
 
     /**
@@ -50,7 +56,11 @@ public class Playlist {
      * @return the duration of the playlist
      */
     private long getTotalDuration() {
-        return soundMap.values().stream().mapToInt(Integer::intValue).sum();
+        return soundMap.values().stream()
+            .flatMap(List::stream)
+            .mapToInt(Integer::intValue)
+            .max()
+            .orElse(0);
     }
 
 }
